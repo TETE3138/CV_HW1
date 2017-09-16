63c621d706507dc8aa914329b1e483872fe363cb
TETE <tewichtete31@hotmail.com>
2017-09-16 11:40:24 +0700

    Add images

--- HW01_Part2_main.cpp
+++ HW01_Part2_main.cpp
@@ -8,7 +8,7 @@ int main()
 {
 	Mat image;
 
-	image = imread("C:/Users/TETE/Desktop/OpenCVFiles/mm2.jpg", IMREAD_COLOR);
+	image = imread("C:/Users/TETE/Desktop/OpenCVFiles/mm5.jpg", IMREAD_COLOR);
 	if (image.empty()) // Check for invalid input
 	{
 		cout << "Could not open or find the image" << std::endl;
@@ -38,6 +38,9 @@ int main()
 	Mat opened_image;
 	Mat SE(4, 4, CV_8U, Scalar(1));
 	morphologyEx(binary, opened_image, MORPH_CLOSE, SE);
+
+	Mat SE2(2, 2, CV_8U, Scalar(1));
+	morphologyEx(binary, opened_image, MORPH_OPEN, SE);
 
 	namedWindow("closed_image", 1);
 	imshow("closed_image", opened_image);
@@ -73,7 +76,8 @@ int main()
 	int yellow = 0;
 	int brown = 0;
 	int green = 0;
-	vector<double> hue(contours.size());
+	vector<double> avg_luminance(contours.size());
+	vector<double> avg_hue(contours.size());
 	for (size_t i = 0; i < contours.size(); i++)
 	{
 		Rect bb = boundingRect(contours[i]);
@@ -83,6 +87,7 @@ int main()
 
 		Vec3i avg_bgr(0, 0, 0);
 		int h = 0;
+		int l = 0;
 		for (size_t y = 0; y < bb.height; y++)
 		{
 			for (size_t x = 0; x < bb.width; x++)
@@ -95,6 +100,7 @@ int main()
 					avg_bgr[2] += image.at<Vec3b>(y + sy, x + sx)[2];*/
 					//result.at<Vec3b>(y + sy, x + sx) = Vec3b(255, 255, 0);
 					h += hls.at<Vec3b>(y + sy, x + sx)[0];
+					l += hls.at<Vec3b>(y + sy, x + sx)[1];
 				}
 
 			}
@@ -103,38 +109,39 @@ int main()
 		/*avg_bgr[0] = avg_bgr[0] / (bb.height*bb.width);
 		avg_bgr[1] = avg_bgr[1] / (bb.height*bb.width);
 		avg_bgr[2] = avg_bgr[2] / (bb.height*bb.width);*/
+		avg_luminance[i] = l / (bb.height*bb.width);
+		avg_hue[i] = h / (bb.height*bb.width);
 
-		hue[i] = h / (bb.height*bb.width);
-
-		if (10 <= hue[i] && hue[i] <= 13) {
+		if (10 <= avg_hue[i] && avg_hue[i] <= 12 && avg_luminance[i] >=65) {
 			orange++;
 			putText(result, "Orange", Point(sx, cy), 1, 1, Scalar(0, 0, 0),2);
 		}
-		else if (0 <= hue[i] && hue[i] <= 9) {
+		else if (0 <= avg_hue[i] && avg_hue[i] <= 9) {
 			red++;
 			putText(result, "Red", Point(sx, cy), 1, 1, Scalar(0, 0, 0), 2);
 		}
-		else if (78 <= hue[i] && hue[i] <= 83) {
+		else if (78 <= avg_hue[i] && avg_hue[i] <= 83) {
 			blue++;
 			putText(result, "Blue", Point(sx, cy), 1, 1, Scalar(0, 0, 0), 2);
 		}
-		else if (14 <= hue[i] && hue[i] <= 17) {
+		else if (12 <= avg_hue[i] && avg_hue[i] <= 17 && avg_luminance[i] <65) {
 			brown++;
 			putText(result, "Brown", Point(sx, cy), 1, 1, Scalar(0, 0, 0), 2);
 		}
-		else if (20 <= hue[i] && hue[i] <= 23) {
+		else if (20 <= avg_hue[i] && avg_hue[i] <= 23) {
 			yellow++;
 			putText(result, "Yellow", Point(sx, cy), 1, 1, Scalar(0, 0, 0), 2);
 		}
-		else if (38 <= hue[i] && hue[i] <= 41) {
+		else if (38 <= avg_hue[i] && avg_hue[i] <= 43) {
 			green++;
 			putText(result, "Green", Point(sx, cy), 1, 1, Scalar(0, 0, 0), 2);
 		}
 
-		//putText(result, to_string(avg_bgr[0]) + ":" + to_string(avg_bgr[1]) + ":" + to_string(avg_bgr[2]), contours[i][0], 1, 1, Scalar(255, 0, 0));
+		
+		//putText(result, to_string(avg_hue[i]), contours[i][0], 1, 1, Scalar(255, 0, 0));
 
-		//putText(result, to_string(i), Point(cx,cy), 1, 1, Scalar(255, 0, 0));
-		printf("%d\t%.2f\n", i, hue[i]);
+		putText(result, to_string(i), Point(cx,cy), 1, 1, Scalar(255, 0, 0));
+		printf("%d\t%.2f\t%.2f\n", i, avg_hue[i],avg_luminance[i]);
 		rectangle(result, bb, Scalar(0, 0, 255), 1);
 		drawMarker(result, Point(cx, cy), Scalar(0, 255, 0));
 
